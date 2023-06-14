fb627b2febf175ce5ce14657369266279f1c73dd
JuYeon Kim <122427007+jupearl@users.noreply.github.com>
2023-06-14 17:21:21 +0900

    Update listup.py

--- utils/listup.py
+++ utils/listup.py
@@ -1,57 +1,55 @@
 
-from collections import Counter
-from datetime import datetime
 import os
-import re
 
 
 def problem_source_code():
     py_problem_solve_code_list = []
     java_problem_solve_code_list = []
 
-    directory_list = [directory for directory in os.listdir("./백준") if ". " in directory]
+    directory_list = [directory for directory in os.listdir("./백준/") if ". " in directory]
 
-    for directory in directory_list:
-        py_code_list = [file for file in os.listdir(f"./백준/{directory}") if file.endswith(".py")]
-        java_code_list = [file for file in os.listdir(f"./백준/{directory}") if file.endswith(".java")]
-        if py_code_list:
-            py_problem_solve_code_list.append(py_code_list[0][:-3])
-        if java_code_list:
-            java_problem_solve_code_list.append(java_code_list[0][:-5])
+
+    # for directory in directory_list:
+    #     py_code_list = [file for file in os.listdir(f"./백준/") if f"{directory}" in file and file.endswith(".py")]
+    #     java_code_list = [file for file in os.listdir(f"./백준/") if file.endswith(".java")]
+    #     if py_code_list:
+    #         py_problem_solve_code_list.append(py_code_list[0][:-3])
+    #     if java_code_list:
+    #         java_problem_solve_code_list.append(java_code_list[0][:-5])
 
     # py_name_list = [re.findall(r'\[[^)]*\]', code_name) for code_name in py_problem_solve_code_list]
     # py_name_list = [name[0].replace("[", "").replace("]", "") for name in py_name_list if len(name) > 0]
     
 
-    return py_problem_solve_code_list, java_problem_solve_code_list
+    return directory_list
+# py_problem_solve_code_list, java_problem_solve_code_list
 
 
-def make_count_info(total_code_num, code_cnt_info):
-    count_info = f"#### 현재까지 풀어본 총 문제 수 : {total_code_num}개\n"
+# def make_count_info(total_code_num, code_cnt_info):
+#     count_info = f"#### 현재까지 풀어본 총 문제 수 : {total_code_num}개\n"
 
-    for name in code_cnt_info:
-        temp = f"- {name[0]} - {name[1]}개\n"
-        count_info += temp
+#     for name in code_cnt_info:
+#         temp = f"- {name[0]} - {name[1]}개\n"
+#         count_info += temp
 
-    return count_info
+#     return count_info
 
 
-def make_read_me(py_name_list, java_name_list):
+def make_read_me(directory_list):
     return f"""# Baekjoon
 <img src="https://img.shields.io/badge/python-3776AB?style=flat&logo=python&logoColor=white"> <img src="https://img.shields.io/badge/-JAVA-007396?style=flat&logo=OpenJDK&logoColor=white"> 
+자동으로 푼 문제 리스트업도 가능하게 매크로 설정해두었음. 
 
-- Python 문제 리스트업
-{py_name_list}
+- 전체 문제 리스트업 
+{','.join(directory_list)}
 
-- Java 문제 리스트업
-{java_name_list}
 """
 
 
 def update_readme_md():
-    py_name_list, java_name_list = problem_source_code()
+    directory_list = problem_source_code()
 
-    readme = make_read_me(py_name_list=py_name_list, java_name_list=java_name_list)
+    readme = make_read_me(directory_list=directory_list)
 
     return readme
 
