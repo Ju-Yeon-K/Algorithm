857f41909f997b89e8f36d22b8b053a1fc379a44
JuYeon Kim <122427007+jupearl@users.noreply.github.com>
2023-06-14 20:34:35 +0900

    Update listup.py

--- utils/listup.py
+++ utils/listup.py
@@ -1,3 +1,4 @@
+
 import os
 
 
@@ -5,7 +6,7 @@ def problem_source_code():
     py_problem_list = []
     java_problem_list = []
 
-    for files in os.walk("../백준/"):
+    for (path, dir, files) in os.walk("./백준/"):
         for filename in files:
             ext = os.path.splitext(filename)[-1]
             if ext == '.py':
@@ -18,7 +19,7 @@ def problem_source_code():
 
 def make_read_me(py_name_list, java_name_list):
     return f"""# Baekjoon
-<img src="https://img.shields.io/badge/python-3776AB?style=flat&logo=python&logoColor=white"> <img src="https://img.shields.io/badge/-JAVA-007396?style=flat&logo=OpenJDK&logoColor=white"> 
+<img src="https://img.shields.io/badge/python-3776AB?style=flat&logo=python&logoColor=white"> <img src="https://img.shields.io/badge/-JAVA-007396?style=flat&logo=OpenJDK&logoColor=white">   
 자동으로 푼 문제 리스트업도 가능하게 매크로 설정해두었음. 
 
 - Python 문제 리스트업   
@@ -39,5 +40,5 @@ def update_readme_md():
 
 if __name__ == "__main__":
     readme = update_readme_md()
-    with open("../README.md", 'w', encoding='utf-8') as f:
+    with open("./README.md", 'w', encoding='utf-8') as f:
         f.write(readme)
