1a561a3549a4b3c661722f6872cab6ce78d5a3a8
JuYeon Kim <122427007+jupearl@users.noreply.github.com>
2023-06-14 21:09:45 +0900

    Update listup.py

--- utils/listup.py
+++ utils/listup.py
@@ -5,38 +5,36 @@ def problem_source_code():
     py_problem_list = []
     java_problem_list = []
 
-    for (path, dir, files) in os.walk("./백준/"):
+    for (path, dir, files) in os.walk("../백준/"):
         for filename in files:
-            if dir:
-                ext = os.path.splitext(filename)[-1]
-                problem_name = dir[-1] # ex) '1034. 부분합'
-                if ext == '.py':
-                    py_problem_list.append(f'[{problem_name}](https://www.acmicpc.net/problem/{int(problem_name.split(". ")[0])})')
-                if ext == '.java':
-                    java_problem_list.append(f'[{problem_name}](https://www.acmicpc.net/problem/{int(problem_name.split(". ")[0])})')
-    
+            ext = os.path.splitext(filename)[-1]
+            problem_name = path.split('/')[-1]
+            
+            if ext == '.py':
+                py_problem_list.append(f'[{problem_name}](https://www.acmicpc.net/problem/{int(problem_name.split(". ")[0])})')
+            if ext == '.java':
+                java_problem_list.append(f'[{problem_name}](https://www.acmicpc.net/problem/{int(problem_name.split(". ")[0])})')
+
     return py_problem_list, java_problem_list
 
 
 def make_read_me(py_name_list, java_name_list):
     return f"""# Baekjoon
 <img src="https://img.shields.io/badge/python-3776AB?style=flat&logo=python&logoColor=white"> <img src="https://img.shields.io/badge/-JAVA-007396?style=flat&logo=OpenJDK&logoColor=white">   
-자동으로 푼 문제 리스트업도 가능하게 매크로 설정해두었음. 
+(푼 문제 자동으로 README.md 업데이트됨.)   
 
-- Python 문제 리스트업   
-    - {'<br>    - '.join(py_name_list)}
-
-- Java 문제 리스트업   
+ ✔️ Python 문제 리스트업   
+    - {'<br>    - '.join(py_name_list)}   
+    
+ ✔️ Java 문제 리스트업   
     - {'<br>    - '.join(java_name_list)}
 
-- ![python](url)|n
 
 """
 
 
 def update_readme_md():
     py_name_list, java_name_list = problem_source_code()
-
     readme = make_read_me(py_name_list=py_name_list, java_name_list=java_name_list)
 
     return readme
