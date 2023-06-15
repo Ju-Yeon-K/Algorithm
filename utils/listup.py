4b9d0524fc5819def7c2280a333c60806c4bbde9
JuYeon Kim <122427007+jupearl@users.noreply.github.com>
2023-06-15 13:05:52 +0900

    Update listup.py

--- utils/listup.py
+++ utils/listup.py
@@ -2,8 +2,10 @@
 import os
 
 def problem_source_code():
-    py_problem_list = []
-    java_problem_list = []
+    b_py_problem_list = []
+    b_java_problem_list = []
+    p_py_problem_list = []
+    p_java_problem_list = []
 
     for (path, dir, files) in os.walk("./백준/"):
         for filename in files:
@@ -11,14 +13,24 @@ def problem_source_code():
             problem_name = path.split('/')[-1]
             
             if ext == '.py':
-                py_problem_list.append(f'[{problem_name}](https://www.acmicpc.net/problem/{problem_name.split(".")[0]}) [Code]({path[1:]}/{problem_name.split(".")[1].lstrip()}.py)')
+                b_py_problem_list.append(f'[{problem_name}](https://www.acmicpc.net/problem/{problem_name.split(".")[0]}) [Code]({path[1:]}/{problem_name.split(".")[1].lstrip()}.py)')
             if ext == '.java':
-                java_problem_list.append(f'[{problem_name}](https://www.acmicpc.net/problem/{problem_name.split(".")[0]}) [Code]({path[1:]}/{problem_name.split(".")[1].lstrip()}.java)')
+                b_java_problem_list.append(f'[{problem_name}](https://www.acmicpc.net/problem/{problem_name.split(".")[0]}) [Code]({path[1:]}/{problem_name.split(".")[1].lstrip()}.java)')
 
-    return sorted(py_problem_list), sorted(java_problem_list)
+    for (path, dir, files) in os.walk("./프로그래머스/"):
+        for filename in files:
+            ext = os.path.splitext(filename)[-1]
+            problem_name = path.split('/')[-1]
+            
+            if ext == '.py':
+                p_py_problem_list.append(f'[{problem_name}](https://www.acmicpc.net/problem/{problem_name.split(".")[0]}) [Code]({path[1:]}/{problem_name.split(".")[1].lstrip()}.py)')
+            if ext == '.java':
+                p_java_problem_list.append(f'[{problem_name}](https://www.acmicpc.net/problem/{problem_name.split(".")[0]}) [Code]({path[1:]}/{problem_name.split(".")[1].lstrip()}.java)')
+    
+    return sorted(b_py_problem_list), sorted(b_java_problem_list), sorted(p_py_problem_list), sorted(p_java_problem_list)
 
 
-def make_read_me(py_name_list, java_name_list):
+def make_read_me(b_py_name_list, b_java_name_list, p_py_name_list, p_java_name_list):
     return f"""# Baekjoon, Programmers 알고리즘 연습
 <img src="https://img.shields.io/badge/python-3776AB?style=flat&logo=python&logoColor=white"> <img src="https://img.shields.io/badge/-JAVA-007396?style=flat&logo=OpenJDK&logoColor=white">   
    
@@ -29,30 +41,30 @@ def make_read_me(py_name_list, java_name_list):
 **1. Baekjoon**   
 
   ✔️ Python 문제 리스트업   
-    - {'<br>    - '.join(py_name_list)}   
+    - {'<br>    - '.join(b_py_name_list)}   
     
 
     
  ✔️ Java 문제 리스트업   
-    - {'<br>    - '.join(java_name_list)}
+    - {'<br>    - '.join(b_java_name_list)}
 
 --- 
 **2. Programmers**   
 
   ✔️ Python 문제 리스트업   
-    - 
+    - {'<br>    - '.join(p_py_name_list)}   
     
 
     
  ✔️ Java 문제 리스트업   
-    - 
+    - {'<br>    - '.join(p_java_name_list)}
 
 """
 
 
 def update_readme_md():
-    py_name_list, java_name_list = problem_source_code()
-    readme = make_read_me(py_name_list=py_name_list, java_name_list=java_name_list)
+    b_py_name_list, b_java_name_list, p_py_name_list, p_java_name_list = problem_source_code()
+    readme = make_read_me(b_py_name_list=b_py_name_list, b_java_name_list=b_java_name_list, p_py_name_list=p_py_name_list, p_java_name_list=p_java_name_list)
 
     return readme
 
