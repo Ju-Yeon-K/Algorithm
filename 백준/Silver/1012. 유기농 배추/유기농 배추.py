6d0a89255ca762f659938e5b52d167ced97d30cd
JuYeon Kim <122427007+jupearl@users.noreply.github.com>
2023-06-15 17:56:09 +0900

    [Silver II] Title: 유기농 배추, Time: 184 ms, Memory: 121440 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,28 +1,29 @@
-def is_1_surround(i, j):
-    global arr
-    dif = [(0,1), (1,0), (0, -1), (-1,0)]
-    arr [i][j] = 2
 
-    for case in dif:
-        temp_i = i+ case[0]
-        temp_j = j+ case[1]
-        if 0 <= temp_i < N and 0 <= temp_j < M and arr[temp_i][temp_j] == 1:
-            is_1_surround(temp_i, temp_j)
-
-
-
-T = int(input())
-for tc in range(1, T+1):
-    M, N, K = map(int,input().split())
-    arr = [[0]*M for _ in range(N)]
-    for _ in range(K):
-        x, y = map(int,input().split())
-        arr[y][x] = 1
+def grouping(i, j): # i, j 로부터 인접한 모든 상추를 2 로 바꿈
+    global area
+    area[i][j] = 2
+    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
     
-    res = 0
+    for direction in directions:
+        next_i = i + direction[0]
+        next_j = j + direction[1]
+        if 0 <= next_i < N and 0 <= next_j < M and area[next_i][next_j] == 1:
+            grouping(next_i, next_j)
+
+
+
+for tc in range(int(input())):
+    M, N, K = map(int, input().split())
+    area = [[0 for _ in range(M)]for _ in range(N)]
+    for _ in range(K):
+        X, Y = map(int, input().split())
+        area[Y][X] = 1
+    
+    cnt = 0
     for i in range(N):
         for j in range(M):
-            if arr[i][j] == 1:
-                is_1_surround(i,j)
-                res += 1
-    print(res)
+            if area[i][j] == 1:
+                grouping(i, j)
+                cnt += 1
+    
+    print(cnt)
